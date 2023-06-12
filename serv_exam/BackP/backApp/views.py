54e0a6f8b30f6cf54e8a87f2d9c9c8f9edf2e888
Erdenebayr2 <94421082+Erdenebayr2@users.noreply.github.com>
2023-06-12 17:13:17 +0800

    Revert "sfd"
    This reverts commit 2a018828a14e78f830b0ac2c48b05f83bec77773.

--- serv_exam/BackP/backApp/views.py
+++ serv_exam/BackP/backApp/views.py
@@ -3,56 +3,55 @@ from django.http import HttpResponse
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 import json
-from FrontP.frontApp import views
 
-# class login_json:
-#     u_login = {}
-#     with open('log_u.json', 'w') as file:
-#         json_data = json.dump(u_login, file)
-# # Create your views here.
-# def index(request):
-#     u_login = login_json.u_login
-#     if request.method == 'POST':
-#         username = request.POST['username']
-#         password = request.POST['password']
-#         age = request.POST['age']
-#         email = request.POST['email']
-#         new_user = {
-#             'name': username,
-#             'password': password,
-#             'age': age,
-#             'email': email
-#         }
-#         json_data = json.dumps(new_user, indent=4)
-#         user_id = 'u_id{}'.format(len(u_login) + 1)
-#         u_login[user_id] = new_user
-#         with open('log_u.json', 'w', encoding='utf-8') as file:
-#             json.dump(u_login, file,indent=4)
-#         print(json_data)
-#         return redirect('log_in')
-#     return render(request, 'index.html')
+class login_json:
+    u_login = {}
+    with open('log_u.json', 'w') as file:
+        json_data = json.dump(u_login, file)
+# Create your views here.
+def index(request):
+    u_login = login_json.u_login
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        age = request.POST['age']
+        email = request.POST['email']
+        new_user = {
+            'name': username,
+            'password': password,
+            'age': age,
+            'email': email
+        }
+        json_data = json.dumps(new_user, indent=4)
+        user_id = 'u_id{}'.format(len(u_login) + 1)
+        u_login[user_id] = new_user
+        with open('log_u.json', 'w', encoding='utf-8') as file:
+            json.dump(u_login, file,indent=4)
+        print(json_data)
+        return redirect('log_in')
+    return render(request, 'index.html')
 
-# def log_in(request):
-#     if request.session.get('json_data'):
-#         return redirect('home')
-#     else:
-#         if request.method == 'POST':
-#             username = request.POST['username']
-#             password = request.POST['password']
-#             with open('log_u.json', 'r') as file:
-#                 json_data = json.dumps(json.load(file))
-#             if json_data:
-#                 user_data = json.loads(json_data)
-#                 print(user_data, type(user_data))
-#                 print(user_data.get('name'))
-#                 if username == user_data['u_id1']['name'] and password == user_data['u_id1']['password']:
-#                     return redirect('home')
-#                 else:
-#                     return HttpResponse('Invalid username or password')
-#         return render(request, 'login.html')
-# def home(request):
-#     return render(request, 'home.html')
-# def log_out(request):
-#     del request.session['json_data']
-#     logout(request)
-#     return redirect('log_in')
+def log_in(request):
+    if request.session.get('json_data'):
+        return redirect('home')
+    else:
+        if request.method == 'POST':
+            username = request.POST['username']
+            password = request.POST['password']
+            with open('log_u.json', 'r') as file:
+                json_data = json.dumps(json.load(file))
+            if json_data:
+                user_data = json.loads(json_data)
+                print(user_data, type(user_data))
+                print(user_data.get('name'))
+                if username == user_data['u_id1']['name'] and password == user_data['u_id1']['password']:
+                    return redirect('home')
+                else:
+                    return HttpResponse('Invalid username or password')
+        return render(request, 'login.html')
+def home(request):
+    return render(request, 'home.html')
+def log_out(request):
+    del request.session['json_data']
+    logout(request)
+    return redirect('log_in')
